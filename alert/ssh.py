097c4b389d79ebf41e7d7a9f0e366c0d1923090c
Javier Quinzaños <quinza1996@gmail.com>
2022-06-16 18:42:38 +0200

    WIP - modulo de deteccion SSH

--- alert/ssh.py
+++ alert/ssh.py
@@ -1,2 +1,14 @@
-def init():
-    print('Init alert/ssh module')
+
+class SSH(Alert):
+
+    def __init__(self):
+        super().__init__()
+        print('Init alert/ssh module')
+
+        # netstat | grep ssh
+
+        get_active_ssh_cons = 'netstat | grep ssh'
+
+        for line in run_command(get_active_ssh_cons):
+            print(line.split())
+
