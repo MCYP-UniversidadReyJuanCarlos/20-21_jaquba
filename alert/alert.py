2a1b7c9c4da03474b452b473066d09403717c842
Javier Quinzaños <quinza1996@gmail.com>
2022-11-19 00:46:50 +0100

    alertas y bot, traduccion para las cadenas, api, etc

--- alert/alert.py
+++ alert/alert.py
@@ -1,25 +1,50 @@
-import subprocess
 import configparser # to read config files
 import os
 from pathlib import Path
-import requests
 import logging # to generate traces of execution
 import subprocess
+import requests
+import socket
+import signal
+import re
 
 #   VARIABLES GLOBALES     #
-logger = None # to log application status and info
-config = configparser.ConfigParser() # to read config file
 
+# to log application status and info
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+debug = True
+config = configparser.ConfigParser() # to read config file
+config.read(Path(__file__).with_name('config.ini'), encoding='iso-8859-1')
 files = []
 
 # Print debug messages
-
-def log(str):
+def log(message):
     global debug, logger
     if debug is False:
-        logger.info(str)
+        logger.info(message)
     else:
-        print(str)
+        print(message)
+
+def is_valid_ipv4_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  # not a valid address
+        return False
+    return True
+
+def is_valid_ipv6_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:  # not a valid address
+        return False
+    return True
 
 class Alert():
     ip: str # ip address of the server which get the alert
@@ -28,29 +53,73 @@ class Alert():
     severity: int # severity of the alert (1 = info, 2 = warning, 3 = error)
     data: str # data of the alert to be sent to the chat
 
-    def run_command(self, command):
+    def run_command(self, command: str):
         p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        return iter(p.stdout.readline, b'')
+        for line in iter(p.stdout.readline, b''):
+            line = line.decode("utf-8").replace('\n', '')
+            line = re.sub(' +', ' ', line)
+            yield line
 
-    def send_alert(self, message):
-        ip = config['botIP']
-        port = config['portIP']
-        url = ip + ':' + port + '/alerts'
-        response = requests.post(url, message)
+    def send_alert(self, message: str):
+        global config
+        ip = config['BOT']['ip']
+        port = config['BOT']['port']
+        url = f'http://{ip}:{port}/alert'
+        log(f'New alert to send {message}')
+        
+        headers = {
+            'User-Agent': 'pythonAlertClass',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        response = requests.post(url, headers=headers, data=message)
         log(response)
+        return response
+
+    def is_valid_ip_address(self, address: str) -> bool:
+        return is_valid_ipv4_address(address) or is_valid_ipv6_address(address)
+
+
+# Invoked when recieves termination signal from user
+def stop_execution(signum, _) -> None:
+    log(f'Recibo signal {signum}')
+    ps = subprocess.Popen(f'ps -o pid --ppid {os.getpid()} --noheaders', shell=True, stdout=subprocess.PIPE)
+    for p in iter(ps.stdout.readline, b''):
+        p = p.decode("utf-8").replace('\n', '')
+        p = re.sub(' +', ' ', p)
+        log(p)
+        os.kill(int(p), signal.SIGTERM)
+
+# Establish signal to catch when exit requested
+def set_signals(handler) -> None:
+    if hasattr(signal, 'CTRL_C_EVENT'):
+        signal.signal(signal.CTRL_C_EVENT, handler)
+    signal.signal(signal.SIGINT, handler)
 
 def main():
-    print('Alert init')
-    config.read(Path(__file__).with_name('config.ini'), encoding='iso-8859-1')
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    logger = logging.getLogger(__name__)
+    global files
+    log('Alert init')
+
+    log('Config file: ' + str(config))
+    log({section: dict(config[section]) for section in config.sections()})
 
     if len(files) == 0:
         base_path = Path(__file__).parent.absolute()
-        print('base_path', base_path)
-
+        log(f'base_path {base_path}')
+        
+        for file_name in base_path.iterdir():
+            if file_name.name.endswith('.py') and not file_name.name.endswith(Path(__file__).name):
+                # add full path, not just file_name
+                file_path = Path(base_path, file_name.name)
+                if file_path not in files:
+                    log(f'New file:\t{file_path}')
+                    files.append(file_path)
+                    # call script
+                    subprocess.run(['python', file_path])
+        
+        '''
         py_file_list = []
-
         for file_name in base_path.iterdir():
             if file_name.name.endswith('.py') and not file_name.name.endswith(Path(__file__).name):
                 # add full path, not just file_name
@@ -59,9 +128,10 @@ def main():
         print('PY files that were found:')
         for i, file_path in enumerate(py_file_list):
             print('\t{:2d} {}'.format(i, file_path))
-            files.append(file_path)
             # call script
             subprocess.run(['python', file_path])
+        '''
 
 if __name__ == '__main__':
+    set_signals(stop_execution)
     main()
--- alert/ssh.py
+++ alert/ssh.py
@@ -1,17 +1,100 @@
 from alert import Alert
+import signal
 import time
+import schedule
+import socket
+from aiocache import Cache # to save connections already seen
+import asyncio # to manage async functions
+import json
+
+class Meta:
+    def __init__(self):
+        pass
+
+    async def get_elem(self, name) -> bool:
+        return await cache.get(name, default=False)
+
+    async def set_elem(self, name, value: bool) -> None:
+        await cache.set(name, value, 1800)
+
+    async def clear(self) -> bool:
+        return await cache.clear()
 
 class SSH(Alert):
 
-    def __init__(self):
-        print('Init alert/ssh module')
-        # netstat | grep ssh
+    def check_connections(self) -> None:
+        global local_ip
         get_active_ssh_cons = 'netstat | grep ssh'
+        lines = self.run_command(get_active_ssh_cons)
 
-        for line in self.run_command(get_active_ssh_cons):
+        for line in lines:
             print(line)
-            #response = this.send_alert(line)
-            #print(response)
-            time.sleep(5)
 
+            remote = line.split(' ')[4]
+            print(f'New connection from: {remote}')
+
+            remote_addr, remote_port = remote.split(':')
+            remote_ip = None
+
+            # check if recieves an IP
+            if self.is_valid_ip_address(remote_addr):
+                remote_ip = remote_addr
+            else:
+                # if not, check for hostname
+                if remote_addr.endswith('.m'):
+                    remote_addr = remote_addr[:remote_addr.index('.m')]
+                print(f'hostname: {remote_addr}')
+
+                # get ip address from hostname
+                remote_ip = socket.gethostbyname(remote_addr)
+            
+            print(f'IP: {remote_ip}')
+
+            if asyncio.run(meta.get_elem(remote)) is False:
+                alert = {
+                    'module_id': 1,
+                    'alert_type': 'SSH',
+                    'severity': 1,
+                    'ip': local_ip,
+                    'data': json.dumps({'ip': remote_ip, 'port': remote_port, 'message': 'Nueva conexion SSH'})
+                }
+
+                json_data = json.dumps(alert)
+
+                response = self.send_alert(json_data)
+                print(response)
+                
+                asyncio.run(meta.set_elem(remote, True))
+
+    def __init__(self):
+        global job_connexions, active
+        print('Init alert/ssh module')
+        job_connexions = schedule.every(5).seconds.do(self.check_connections)
+
+        while active:
+            schedule.run_pending()
+            time.sleep(1)
+
+#####     VARIBLES GLOBALES     #####
+#####################################
+cache = Cache(Cache.MEMORY)
+meta = Meta()
+
+active = True
+local_ip = socket.gethostbyname(socket.gethostname())
+job_connexions = None
+#####################################
+
+# Invoked when recieves termination signal from user
+def stop_execution(signum, frame) -> None:
+    global active, job_connexions
+    print('Recibo signal ' + str(signum))
+    active = False
+    schedule.cancel_job(job_connexions)
+    asyncio.run(meta.clear())
+
+# Starts listening
 SSH().__init__()
+
+# Establish signal to catch when exit requested
+signal.signal(signal.SIGTERM, stop_execution)
